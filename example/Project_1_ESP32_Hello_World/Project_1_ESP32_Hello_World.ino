6539c0b5fff3168be8d1b83229c9e199a5ee799a
Han Chul. Kim <rainnam@gmail.com>
2024-07-04 18:26:16 +0900

    소스 설명 수정함.

--- example/Project_1_ESP32_Hello_World/Project_1_ESP32_Hello_World.ino
+++ example/Project_1_ESP32_Hello_World/Project_1_ESP32_Hello_World.ino
@@ -1,14 +1,14 @@
-char val; // defines variable “val”
+char val; // “val” 변수 정의 
 
 void setup() {
-     Serial.begin(115200); //sets baudrate to 115200
+     Serial.begin(115200); // 115200 로 시리얼 속도 설정 
 } 
 
 void loop() { 
    if (Serial.available() > 0) {
-      val=Serial.read(); //reads symbols assigns to “val”
-     if (val == ‘L’) { //check input for the letter “L”
-          Serial.println(“Hello World!”); //shows “Hello World!”
+      val=Serial.read(); //“val” 에 시리얼로 부터 읽은 값 저장
+     if (val == 'L') { //읽은 값이 "L" 인지 확인함. 
+          Serial.println("Hello World!"); // L 이 맞다면 “Hello World!” 출력
      }
    }
 }
